5824c46c9aff20f4a90fb5789223190ab5a605b1
zenilsonneto07-arch <zenilsonneto07@gmail.com>
2026-03-06 14:03:24 -0300

    Acionamos a page do desenvolvedor

--- Js/index.js
+++ Js/index.js
@@ -3,3 +3,30 @@
 function Semitem(){
     alert("Item esgotado")
 }
+
+function Enviar() {
+    document.body.style.backgroundColor="greenyellow";
+    
+}
+function viraVermelho(){
+    let a = document.getElementById("teste");
+    a.style.backgroundColor = "red"
+}
+
+function viraAzul(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "greenyellow"
+}
+
+function Aczul (){
+let a = document.getElementById("Acessorio");
+    a.style.backgroundColor = "red"
+}
+function Acev (){
+ let a = document.getElementById("Acessorio");
+    a.style.backgroundColor = "greenyellow"
+}
+
+function doisclicks(){
+  alert("Item esgotado")  
+}
