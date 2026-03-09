83432071984eebce9efc07ea43c594bd139271f0
zenilsonneto07-arch <zenilsonneto07@gmail.com>
2026-03-09 18:22:57 -0300

    Foi adicionada funcao de detalhe nas vendas

--- Js/index.js
+++ Js/index.js
@@ -30,3 +30,16 @@ function Acev (){
 function doisclicks(){
   alert("Item esgotado")  
 }
+
+var hora = new Date().getHours();
+
+
+if ( hora <12) {
+    alert ("Bom dia")
+}else if(hora <18){
+    alert("Boa tarde")
+}else{
+    alert("Boa noite")
+}
+
+
