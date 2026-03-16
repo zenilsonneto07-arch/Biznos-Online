b97487deb56d3c4c370399cf12a2aface4078016
zenilsonneto07-arch <zenilsonneto07@gmail.com>
2026-03-16 18:06:54 -0300

    Adicao de outras coisas

--- Js/index.js
+++ Js/index.js
@@ -34,12 +34,5 @@ function doisclicks(){
 var hora = new Date().getHours();
 
 
-if ( hora <12) {
-    alert ("Bom dia")
-}else if(hora <18){
-    alert("Boa tarde")
-}else{
-    alert("Boa noite")
-}
 
 
