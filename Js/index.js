ee1c1209cab5c7b62a0d8ecb89a9f43e8d976004
zenilsonneto07-arch <zenilsonneto07@gmail.com>
2026-03-21 10:27:32 -0300

    Adiçao de js nos botoes dos artigos

--- Js/index.js
+++ Js/index.js
@@ -4,39 +4,49 @@ function Semitem(){
     alert("Item esgotado")
 }
 
-function Enviar() {
-    document.body.style.backgroundColor="greenyellow";
-    
-}
+
 function viraVermelho(){
-    let a = document.getElementById("teste");
-    a.style.backgroundColor = "red"
+   document.getElementById("teste").style.backgroundColor = "red"
 }
 
 function viraAzul(){
     let div = document.getElementById("teste");
     div.style.backgroundColor = "greenyellow"
+    
 }
 
 function Aczul (){
-let a = document.getElementById("Acessorio");
-    a.style.backgroundColor = "red"
+let a = document.getElementById("ceste").style.backgroundColor = "red"
 }
 function Acev (){
- let a = document.getElementById("Acessorio");
+ let a = document.getElementById("ceste");
     a.style.backgroundColor = "greenyellow"
 }
 
-function doisclicks(){
-  alert("Item esgotado")  
+function Tocas (){
+let a = document.getElementById("deste").style.backgroundColor = "red"
+}
+function Chapeus (){
+ let a = document.getElementById("deste");
+    a.style.backgroundColor = "greenyellow"
 }
 
-var hora = new Date().getHours();
-
-function onclick (){
-
-if (li >= 10px)
-    
+function T_Shirts (){
+let a = document.getElementById("este").style.backgroundColor = "red"
 }
+function T_Shirt (){
+ let a = document.getElementById("este");
+    a.style.backgroundColor = "greenyellow"
+}
+
+function Tenis (){
+let a = document.getElementById("feste").style.backgroundColor = "red"
+}
+function Teni(){
+ let a = document.getElementById("feste");
+    a.style.backgroundColor = "greenyellow"
+}
+
+
 
 
